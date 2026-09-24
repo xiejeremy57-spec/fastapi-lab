b4f56a512e7f95f837e11eb6db50b68d5a5e82fa
xiejeremy57-spec <xiejeremy57@gmail.com>
2026-09-24 21:29:18 +0800

    ahdjkhaj

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ app = FastAPI()
 @app.get("/")
 async def index():
     return {"message": "Hello FastAPI"}
+@app.get("/users/{user_id}")
+async def user_profile(user_id:int):
+    return {
+        "id":user_id,
+        "name":"zhangsan"
+    }
